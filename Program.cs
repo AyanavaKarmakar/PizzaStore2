08f0f93fc47a7bdf3dc1655c66e5c4937d9d98d9
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-02 23:58:16 +0530

    feat: add swagger ui

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Title = "PizzaStore API",
+        Description = "Making the pizzas you love",
+        Version = "v2"
+
+    });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v2/swagger.json", "PizzaStore API v2");
+});
 
 app.MapGet("/", () => "Hello World!");
 
