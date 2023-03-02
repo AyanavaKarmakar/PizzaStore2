b668f682654459a4c22a6539ebc8203a75e3c9c6
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-03 00:39:29 +0530

    feat: connect db & migrate

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using Microsoft.EntityFrameworkCore;
 using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
+
+builder.Services.AddSqlite<PizzaDB>(connectionString);
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v2", new OpenApiInfo
