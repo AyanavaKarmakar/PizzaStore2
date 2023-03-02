e38c0ebeca7c3e64858e30bbcdf11871f3490f4b
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-03 00:11:19 +0530

    feat: add EF Core

--- Pizza.cs
+++ Pizza.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace PizzaStore.Models
 {
     public class Pizza
@@ -5,5 +7,11 @@ namespace PizzaStore.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+    }
+
+    class PizzaDB : DbContext
+    {
+        public PizzaDB(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v2", new OpenApiInfo
@@ -22,6 +25,6 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v2/swagger.json", "PizzaStore API v2");
 });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas", async (PizzaDB db) => await db.Pizzas.ToListAsync());
 
 app.Run();
